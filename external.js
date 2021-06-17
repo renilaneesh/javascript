82ccc86e40e8038012b257a876a9b604e05209df
renilaneesh <renilaneesh999@gmail.com>
2021-06-17 11:52:26 +0530

    updated

---
+++ external.js
@@ -0,0 +1,36 @@
+// document.writeln("<b><h1>External linked file</h1></b>");
+// var name = "Renil";
+// let age = 29;
+// const pi = 3.14;
+// document.writeln(name,"<br>",age,"<br>"); 
+// document.writeln(typeof(age),"<br>");
+// document.writeln(typeof(name),"<br>");
+// document.write(typeof pi,"<br>");
+// let student={
+//     nam:'renil',roll:17,iseligible:true
+//             }
+// document.write(student.nam);
+// console.log(student);
+// var month=['jan','feb','mar','aprl','may','june'];
+// document.write(month[3]);
+// var num=[10,20,30,40,50,60,70,80,90,100];
+// console.log(num[2]);
+// function welcome()
+// {
+//     document.write("<b>hello,renil here</b>");
+// }
+// welcome();
+let x=20,y=30;
+function add(a,b)
+{
+    let c= a+b;
+    document.write("answer of addition is "+c);
+}
+add(x,y);
+
+function hy()
+{
+    let m= "your name";
+    document.write(m);
+    alert("we got answer "+m);
+}
---
+++ foundation.js
@@ -0,0 +1,88 @@
+// let a = 2;
+// let b = 3;
+
+// add = a + b;
+// console.log(add);
+
+// let sub = a-b;
+// console.log(sub);
+ 
+// let mul=a*b;
+// console.log(mul);
+
+// let div=a/b;
+// console.log(div);
+
+// let mod=a%b;
+// console.log(mod);
+
+// a++;
+// console.log(a);
+// b--;
+// console.log(b);
+
+// let a = 1;
+// let b = a++;
+// console.log(b);
+// let c = ++a;
+// console.log(c);
+
+// let a=1;
+// let b=5;
+// console.log(a==b);
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a!=b);
+// console.log(a<=b);
+// console.log(a>=b);
+
+// let a=3;
+// let b=4;
+// console.log(a+=3);
+// console.log(a-=2);
+// console.log(a*=b);
+
+// let a=true;
+// let b=false;
+
+// console.log(a&&b);
+// console.log(a||b);
+// console.log(!a);
+
+// let a=111;
+// let b=010;
+
+// console.log(a&b);
+ 
+// let a=2;
+// let b=3;
+// let c=7;
+
+// console.log(a+c*b);
+// console.log((a+b)*c);
+
+// if(5<10)
+// {
+//     console.log("5 is lesser");
+
+// }
+// else{
+//     console.log("false");
+// }
+
+// for(let i=1;i<10;i++)
+// {
+//     console.log(i);
+// }
+//  let i=0;
+
+// while(i<10)
+// {
+//     console.log(i);
+//     i++;
+// }
+let i=1;
+do{
+    console.log(i);
+    i++;
+}while(i<10);
